0a43662588f42218eb00307caf607f56e4758bfe
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-13 13:53:31 +0900

    [Gold IV] Title: 도서관, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -1,64 +1,25 @@
-n, m = tuple(map(int, input().split()))
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 
-a = list(map(int, input().split()))
-a.sort()
+neg = []
+pos = []
 
-left_a = [elem for elem in a if elem < 0]
-right_a = [elem for elem in a if elem > 0]
-
-new_right_a = []
-pack = 0
-for i in range(len(right_a)-1, -1, -1):
-    if pack == 0:
-        new_right_a.append(right_a[i])
-    
-    pack += 1
-
-    if pack == m:
-        pack = 0
-
-new_left_a = []
-pack = 0
-for i in range(0, len(left_a)):
-    if pack == 0:
-        new_left_a.append(left_a[i])
-    
-    pack += 1
-
-    if pack == m:
-        pack = 0
-
-# print(left_a)
-# print(right_a)
-
-# print(new_left_a)
-# print(new_right_a)
-
-ans = 0
-
-for i in range(len(new_right_a)):
-    if i == 0:
-        ans += new_right_a[i]
+for x in arr:
+    if x > 0:
+        pos.append(x)
     else:
-        ans += new_right_a[i] * 2
+        neg.append(-x)
 
-for i in range(len(new_left_a)):
-    if i == 0:
-        ans += abs(new_left_a[i]) 
-    else:
-        ans += abs(new_left_a[i]) * 2
+pos = sorted(pos)[::-1]
+neg = sorted(neg)[::-1]
 
-if len(new_left_a) == 0:
-    print(ans)
-    exit(0)
-elif len(new_right_a) == 0:
-    print(ans)
-    exit(0) 
+dists = []
 
-if abs(new_left_a[0]) < new_right_a[0]:
-    ans += abs(new_left_a[0])
-else:
-    ans += new_right_a[0]
+for p in pos[::M]:
+    dists.append(p)
 
-print(ans)
+for n in neg[::M]:
+    dists.append(n)
+
+print(sum(dists) * 2 - max(dists))
 

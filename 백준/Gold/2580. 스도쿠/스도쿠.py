c163242d201583fcfbdcdde2b30e64a71849349f
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2024-10-31 09:34:22 +0900

    [Gold IV] Title: 스도쿠, Time: 1328 ms, Memory: 115444 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,21 +1,5 @@
-def is_possible(y, x, n):
-	global matrix
-
-	for c in range(9):
-		if matrix[y][c] == n:
-			return False
-	for r in range(9):
-		if matrix[r][x] == n:
-			return False
-	for r in range(3):
-		for c in range(3):
-			if matrix[3 * (y // 3) + r][3 * (x // 3) + c] == n:
-				return False
-
-	return True
-
 def search(lev):
-	global matrix, pos
+	global row, col, square, matrix, pos
 
 	# base case
 	if lev == len(pos): # 스도쿠 완성
@@ -24,17 +8,29 @@ def search(lev):
 				print(matrix[i][j], end=' ')
 			print()
 		exit(0)
-		return
 
 	y, x = pos[lev]
 
 	# recursive case
 	for n in range(1, 10):
-		if is_possible(y, x, n):
+		if (n not in row[y]) and (n not in col[x]) and (n not in square[y // 3][x // 3]):
+			row[y].add(n)
+			col[x].add(n)
+			square[y // 3][x // 3].add(n)
 			matrix[y][x] = n
-			search(lev + 1)
-			matrix[y][x] = 0
 
+			search(lev + 1)
+
+			matrix[y][x] = 0
+			square[y // 3][x // 3].remove(n)
+			col[x].remove(n)
+			row[y].remove(n)
+
+
+#initial settings
+row = [set() for _ in range(9)]
+col = [set() for _ in range(9)]
+square = [[set() for _ in range(3)] for _ in range(3)]
 
 # input
 matrix = [list(map(int, input().split())) for _ in range(9)]
@@ -46,5 +42,9 @@ for i in range(9):
 		cur = matrix[i][j]
 		if cur == 0:
 			pos.append((i, j))
+		else:
+			row[i].add(cur)
+			col[j].add(cur)
+			square[i // 3][j // 3].add(cur)
 
 search(0)

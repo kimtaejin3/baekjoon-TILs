669b5c14325cc3f72dab016a0b47e0301fa0c520
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2025-01-29 18:11:12 +0900

    [Gold III] Title: 파티, Time: 268 ms, Memory: 40840 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,24 +1,18 @@
 from queue import PriorityQueue
 
-N, M, X = map(int, input().split())
-
 INF = int(1e12)
-adj_list = [[] for _ in range(N + 1)]
-dist_list = [[] for _ in range(N + 1)]
 
-for _ in range(M):
-	a, b, t = map(int, input().split())
-	adj_list[a].append([b, t])
+def dijkstra(adj_list, start_node):
+	global N
 
-def dijkstra(node):
 	dist = [INF] * (N + 1)
+
 	pq = PriorityQueue()
-	pq.put([0, node])
-	dist[node] = 0
+	dist[start_node] = 0
+	pq.put([0, start_node])
 
 	while not pq.empty():
 		cur_dist, cur_node = pq.get()
-
 		for adj_node, adj_dist in adj_list[cur_node]:
 			temp_dist = cur_dist + adj_dist
 			if temp_dist < dist[adj_node]:
@@ -27,12 +21,23 @@ def dijkstra(node):
 
 	return dist
 
-ans = -1
-for town in range(1, N + 1):
-	dist_list[town] = dijkstra(town)
+# input
+N, M, X = map(int, input().split())
 
-for town in range(1, N + 1):
-	temp = dist_list[town][X] + dist_list[X][town]
-	ans = max(ans, temp)
+adj_list1 = [[] for _ in range(N + 1)]
+adj_list2 = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+	a, b, t = map(int, input().split())
+	adj_list1[a].append([b, t])
+	adj_list2[b].append([a, t])
+
+# solve (dijkstra)
+dist1 = dijkstra(adj_list1, X)
+dist2 = dijkstra(adj_list2, X)
+
+ans = -1
+for i in range(1, N + 1):
+	ans = max(ans, dist1[i] + dist2[i])
 
 print(ans)
